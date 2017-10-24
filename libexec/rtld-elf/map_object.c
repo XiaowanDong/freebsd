1d54e4fcc5592afd3889dab5dde7d6fd8558cea1
XiaowanDong <xdong@cs.rochester.edu>
2017-10-24 15:05:57 -0500

    added notes for sharing page table for shared libraries

--- libexec/rtld-elf/map_object.c
+++ libexec/rtld-elf/map_object.c
@@ -218,7 +218,7 @@ map_object(int fd, const char *path, const struct stat *sb)
 	data_flags = convert_flags(segs[i]->p_flags) | MAP_FIXED;
 	if (mmap(data_addr, data_vlimit - data_vaddr, data_prot,
 	  data_flags | MAP_PREFAULT_READ |
-	  ((data_prot & PROT_WRITE) ? 0 : MAP_SHAREPT),
+	  ((data_prot & PROT_WRITE) ? 0 : 0x2000), //xdong 1. (baseline) 0: MAP_SHAREPT -> 0:0 2. (shared_ptp) MAP_SHAREPT -> 0x2000
 	  fd, data_offset) == (caddr_t) -1) {
 	    _rtld_error("%s: mmap of data failed: %s", path,
 		rtld_strerror(errno));
